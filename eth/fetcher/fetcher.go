6ad8bdd0f74aeedc74a3773f9e6d2a0f7b2e33b5
jason jiang <czjs2@qq.com>
2019-01-12 16:44:57 +0800

    Update fetcher.go
    blockLimit change

--- eth/fetcher/fetcher.go
+++ eth/fetcher/fetcher.go
@@ -38,7 +38,7 @@ const (
 	maxUncleDist  = 512                      // Maximum allowed backward distance from the chain head
 	maxQueueDist  = 512                     // Maximum allowed distance from the chain head to queue
 	hashLimit     = 256                    // Maximum number of unique blocks a peer may have announced
-	blockLimit    = 64                     // Maximum number of unique blocks a peer may have delivered
+	blockLimit    = 512                     // Maximum number of unique blocks a peer may have delivered
 )
 
 var (
